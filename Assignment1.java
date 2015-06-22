101ff30287ca4ae47f8cb868d1a531b67fefcc00
subho-banerjee <banerjee.s2207@gmail.com>
2015-06-22 19:54:30 +0530

    Assignment 1

---
+++ Assignment1.java
@@ -0,0 +1,25 @@
+
+public class Assignment1 {
+
+	static byte a = 1;
+	static char b = 'a';
+	static int c = 2;
+	static float d = 3.5f;
+	static double e = 4.57;
+	static long f = 1000;
+	static short g = 23;
+	static boolean h = true;
+	
+	public static void main(String[] args) {
+		System.out.println("The byte value is "+a);
+		System.out.println("The char value is "+b);
+		System.out.println("The integer value is "+c);
+		System.out.println("The float value is "+d);
+		System.out.println("The double value is "+e);
+		System.out.println("The long value is "+f);
+		System.out.println("The short value is "+g);
+		System.out.println("The boolean value is "+h);
+
+	}
+
+}
